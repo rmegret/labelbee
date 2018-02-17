56dbaaa2d750afcb640275c4633334f463beeba0
Remi Megret <remi.megret@upr.edu>
2018-02-16 21:27:04 -0400

    - workaround: runserver.sh multithreaded to avoid long response delay

--- runserver.sh
+++ runserver.sh
@@ -3,4 +3,5 @@
 # Run a Flask development server
 # --debug       # Run it in DEBUG mode.
 # --reload      # Restart when a python file changed
-python manage.py runserver --debug --reload
+python manage.py runserver --debug --reload --threaded
+
