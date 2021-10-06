ef0e36bbc8648e4e39622ad9a4f038ac1cde6ceb
Daniel Suazo <danielsuazo9@gmail.com>
2021-10-06 13:17:24 -0400

    midweek update

--- labelbee/user_management.py
+++ labelbee/user_management.py
@@ -1,6 +1,23 @@
 from labelbee.models import User, Role, UsersRoles
 from labelbee.init_app import db, app
+from typing import Tuple
 from datetime import datetime
+
+
+def import_users(csv_file: str) -> None:
+    with open(csv_file, "r") as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            first_name, last_name, email, password, role_id = line.split(",")
+            create_user(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                password=password,
+                role_id=role_id,
+            )
 
 
 def change_user_role(user_id: int, role_id: int) -> None:
@@ -12,7 +29,7 @@ def change_user_role(user_id: int, role_id: int) -> None:
 
 def create_user(
     first_name: str, last_name: str, email: str, password: str, role_id: int = 2
-) -> None:
+) -> User:
     user = User(
         first_name=first_name,
         last_name=last_name,
@@ -26,6 +43,7 @@ def create_user(
     user_role = UsersRoles(user_id=user.id, role_id=role.id)
     db.session.add(user_role)
     db.session.commit()
+    return user
 
 
 def edit_user(
@@ -35,7 +53,7 @@ def edit_user(
     email: str,
     password: str,
     role_id: int,
-) -> None:
+) -> Tuple[User, UsersRoles]:
     user = User.query.get(user_id)
     user.first_name = first_name if first_name else user.first_name
     user.last_name = last_name if last_name else user.last_name
@@ -47,3 +65,4 @@ def edit_user(
     user_role = UsersRoles.query.filter_by(user_id=user_id).first()
     user_role.role_id = role_id
     db.session.commit()
+    return user, user_role
