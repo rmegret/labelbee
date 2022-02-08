462be3ed483d01f1b13271423eccd3bcd90385fb
Joel <joelmgh@gmail.com>
2022-02-08 06:00:39 -0400

    More error message fixes

--- labelbee/static/labelbee/js/VideoList.js
+++ labelbee/static/labelbee/js/VideoList.js
@@ -512,7 +512,7 @@ VideoManager.prototype.videoListFromDB = function () {
   // fromServerDialog.closeDialog();
   if (this.videoListJSON == "Error"){
     fromServerDialog.setBody("<button onclick='videoManager.receiveVideoSelection();'>Refresh Video List</button><br>");
-    fromServerDialog.setMessage("red", "VideoManager.receiveVideoSelection ERROR: Unable to retrieve video list from server.<br>Press the reload button to try again.");
+    fromServerDialog.setMessage("red", "VideoManager.receiveVideoSelection ERROR: Unable to retrieve video list from server.<br>Press the refresh button above to try again.");
   }
   else if (this.videoListJSON != null){
     this.makeVideoListTable(this.videoListJSON);
@@ -571,7 +571,7 @@ VideoManager.prototype.receiveVideoSelection = function(){
     error: function (){
         // Display error message in dialog menu
         fromServerDialog.setBody("<button onclick='videoManager.receiveVideoSelection();'>Refresh Video List</button><br>"); 
-        fromServerDialog.setMessage("red", "VideoManager.receiveVideoSelection ERROR: Unable to retrieve video list from server.");
+        fromServerDialog.setMessage("red", "VideoManager.receiveVideoSelection ERROR: Unable to retrieve video list from server.<br>Press the refresh button above to try again.");
         theManager.videoListJSON = "Error";
       },
     success: function(json){
