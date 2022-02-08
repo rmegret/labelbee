e25cd5fe0976d8036eeab8414ef89fd9b78eef47
Joel <joelmgh@gmail.com>
2022-02-07 23:26:31 -0400

    Fixed start script to start test server

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-source /home/dsuazo/flasktest/labelbee/env/bin/activate
+export SECRET_KEY=XXUu3fPg33ctsLX-JSyx0w
+export DB_URL=136.145.54.43/videos
+export DB_PASSWORD=9gji1=Y.6?4vJy
+export DB_USER=labelbeeflask
 
-cd /home/dsuazo/flasktest/labelbee
+source /var/www/flasktest/labelbee/bin/env/activate
 
-gunicorn -p labelbee.pid -b 127.0.0.1:8090 manage:app -w 1 --threads 12 --timeout 300 --log-file /home/dsuazo/flasktest/labelbee/gunicorn_error_logs.log --daemon
+cd /var/www/flasktest/labelbee/
 
+gunicorn -p labelbee.pid -b 127.0.0.1:8090 manage:app -w 1 --threads 12 --timeout 300 --log-file /var/www/flasktest/labelbee/gunicorn_error_logs.log --daemon
