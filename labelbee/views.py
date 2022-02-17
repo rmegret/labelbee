f51484a9e74e0da19560fa66569816569f846d94
Joel <joelmgh@gmail.com>
2022-02-17 12:44:31 -0400

    Testing print output

--- labelbee/views.py
+++ labelbee/views.py
@@ -1048,8 +1048,6 @@ def add_video_data_v2():
     form_data = request.form
     # form_data = json.dumps(form_data)
     print(f"Information obtained from POST request: {form_data}")
-    app.logger.error(f"Information obtained from POST request: {form_data}")
-    app.logger.debug(f"Information obtained from POST request: {form_data}")
     newdata = video_data_schema.loads(form_data)
 
     if "video_id" not in newdata:
