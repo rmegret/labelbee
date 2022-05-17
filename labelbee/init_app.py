7e1c79ae87ea8344bb8b8680ef240ab8e20048a8
Remi Megret <remi.megret@upr.edu>
2022-05-16 21:26:35 -0400

    Fixed registration functionality Fixed errors throughout jinja templates

--- labelbee/init_app.py
+++ labelbee/init_app.py
@@ -142,7 +142,6 @@ def init_app(app, extra_config_settings={}):
     from labelbee.views import user_profile_page
 
     user_manager = CustomUserManager(app, db, User)
-
     from labelbee.db_functions import injest_tags, injest_videos
 
     # try:
@@ -183,18 +182,15 @@ def init_email_error_handler(app):
     subject = app.config.get("APP_SYSTEM_ERROR_SUBJECT_LINE", "System Error")
 
     # Setup an SMTP mail handler for error-level messages
-    # import logging
-    # from logging.handlers import SMTPHandler
-
-    # mail_handler = SMTPHandler(
-    #     mailhost=(host, port),  # Mail host and port
-    #     fromaddr=from_addr,  # From address
-    #     toaddrs=to_addr_list,  # To address
-    #     subject=subject,  # Subject line
-    #     credentials=(username, password),  # Credentials
-    #     secure=secure,
-    # )
-    # mail_handler.setLevel(logging.ERROR)
-    # app.logger.addHandler(mail_handler)
+    mail_handler = logging.handlers.SMTPHandler(
+        mailhost=(host, port),  # Mail host and port
+        fromaddr=from_addr,  # From address
+        toaddrs=to_addr_list,  # To address
+        subject=subject,  # Subject line
+        credentials=(username, password),  # Credentials
+        secure=secure,
+    )
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
 
     # Log errors using: app.logger.error('Some error message')
