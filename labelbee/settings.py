3f0a9560b9af15ae3a6710af564a9cb59caf0ca3
Remi Megret <remi.megret@upr.edu>
2022-05-28 15:29:26 -0400

    Disabled email confirmations for all actions No SMTP service available and no longer required

--- labelbee/settings.py
+++ labelbee/settings.py
@@ -16,7 +16,7 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Flask-User settings
 USER_APP_NAME = APP_NAME
-USER_ENABLE_CHANGE_PASSWORD = False  # Allow users to change their password
+USER_ENABLE_CHANGE_PASSWORD = True  # Allow users to change their password
 USER_ENABLE_CHANGE_USERNAME = False  # Allow users to change their username
 USER_ENABLE_CONFIRM_EMAIL = False  # Force users to confirm their email during registration
 USER_ENABLE_FORGOT_PASSWORD = False  # Allow users to reset their passwords
@@ -28,8 +28,9 @@ USER_ENABLE_REMEMBER_ME = False
 USER_AFTER_LOGIN_ENDPOINT = 'user_page'
 USER_AFTER_LOGOUT_ENDPOINT = 'home_page'
 USER_EMAIL_SENDER_NAME = USER_APP_NAME
-USER_EMAIL_SENDER_EMAIL = "noreply@example.com"
+# USER_EMAIL_SENDER_EMAIL = "noreply@example.com"
 USER_SEND_REGISTERED_EMAIL = False # Send an email to user confirming registration
+USER_SEND_PASSWORD_CHANGED_EMAIL = False
 
 USER_AFTER_CHANGE_PASSWORD_ENDPOINT = 'user_page'
 USER_AFTER_CHANGE_USERNAME_ENDPOINT = 'user_page'
