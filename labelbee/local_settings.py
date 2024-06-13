1a1b55409a2fee97a1a4feea4a4ebf555e702ae4
Andres-ramos <ramosandres443@gmail.com>
2024-06-13 12:40:52 -0400

    Updated settings, gitignore and requirements

--- labelbee/local_settings.py
+++ labelbee/local_settings.py
@@ -4,33 +4,18 @@ import sys
 # *****************************
 # Environment specific settings
 # *****************************
+from dotenv import load_dotenv
 
-# DO NOT use "DEBUG = True" in production environments
-try:
-    DEBUG = os.environ["DEBUG"]
-except KeyError:
-    DEBUG = False
-
-# DO NOT use Unsecure Secrets in production environments
-# Generate a safe one with:
-#     python -c "import os; print repr(os.urandom(24));"
-try:
-    SECRET_KEY = os.environ[
-        "SECRET_KEY"
-    ]  # "This is an UNSECURE Secret. CHANGE THIS for production environments."
-except KeyError:
-    sys.exit("Environment variable SECRET_KEY not set.")
+load_dotenv()
 
 WTF_CSRF_TIME_LIMIT = None
-
-# SQLAlchemy settings
-# SQLALCHEMY_DATABASE_URI = "sqlite:///private/app.sqlite"
-try:
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.environ['DB_USER']}:{os.environ['DB_PASSWORD']}@{os.environ['DB_URL']}"
-except KeyError:
-    sys.exit("Environment variables DB_USER, DB_PASSWORD, and DB_URL Required")
+DEBUG = os.environ["DEBUG"]
 
 # Flask-Mail settings (use local server)
+SECRET_KEY = os.environ[
+        "SECRET_KEY"
+    ] 
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:password@localhost:3306/labelbee-db"
 MAIL_SERVER = "localhost"
 MAIL_PORT = 25
 MAIL_USE_SSL = False
