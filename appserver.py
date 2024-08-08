42ac6597e9fbad89d60a000b16f92a00e15bc014
Andres-ramos <ramosandres443@gmail.com>
2024-08-07 22:05:32 -0400

    fixed deployment script

---
+++ appserver.py
@@ -0,0 +1,8 @@
+from labelbee.app import create_app
+
+if __name__ == '__main__':
+    gunicorn_app = create_app()
+    gunicorn_app.run()
+else:
+    gunicorn_app = create_app()
+
--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -13,24 +13,17 @@ checkEnvVariable() {
 # Define all vars in a script and source it
 # source gunicorn_config.sh
 
-checkEnvVariable SECRET_KEY
-checkEnvVariable DB_URL
-checkEnvVariable DB_PASSWORD
-checkEnvVariable DB_USER
-
 checkEnvVariable LABELBEE_PORT
 checkEnvVariable LABELBEE_APPDIR
 checkEnvVariable LABELBEE_LOGDIR
-
-checkEnvVariable CONDA_EXE
 
 GUNICORN_PIDFILE="$LABELBEE_LOGDIR"/labelbee.pid
 GUNICORN_LOG="$LABELBEE_LOGDIR"/gunicorn_error_logs.log
 
 # Setup CONDA, then activate labelbee env
-eval "$($CONDA_EXE shell.bash hook)"
-conda activate labelbee
+
+source venv/bin/activate
 
 # May be a good idea to check https://docs.gunicorn.org/en/latest/configure.html
 
-gunicorn -p "$GUNICORN_PIDFILE" -b 127.0.0.1:$LABELBEE_PORT --chdir "$LABELBEE_APPDIR" manage:app -w 1 --threads 12 --timeout 300 --log-file "$GUNICORN_LOG" --daemon
+gunicorn -p "$GUNICORN_PIDFILE" -b 127.0.0.1:$LABELBEE_PORT --chdir "$LABELBEE_APPDIR" appserver:gunicorn_app --log-file "$GUNICORN_LOG" -w 1 --threads 12 --timeout 300 --daemon
